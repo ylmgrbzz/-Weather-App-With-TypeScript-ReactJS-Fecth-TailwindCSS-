91da7cfa3372e14c96c0dd30f44ac4d8192ed964
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-03-12 13:57:50 +0300

    Weather App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-const App = () => {
+import Forecast from './components/Forecast'
+import Search from './components/Search'
+
+import useForecast from './hooks/useForecast'
+
+const App = (): JSX.Element => {
+  const { forecast, options, term, onOptionSelect, onSubmit, onInputChange } =
+    useForecast()
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
